3a0f4ff17d0269a01f47d528f345d314963ed03a
Humberto <hmuntista@gmail.com>
2013-03-29 17:41:58 -0600

    calles nuevas

--- Semaforo/Form1.cs
+++ Semaforo/Form1.cs
@@ -65,112 +65,174 @@ namespace Semaforo
         {
             while (true)
             {
-                //Thread.Sleep(200);
+                
                // for (int i = 0; i < 15;i++)
                 //{
-                this.pictureBox2.Image = global::Semaforo.Properties.Resources.sem_verde;
-                this.pictureBox3.Image = global::Semaforo.Properties.Resources.sem_verde;
                 this.pictureBox18.Image = global::Semaforo.Properties.Resources.sem_verde;
-                Thread.Sleep(400);
+                this.pictureBox2.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                this.pictureBox3.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                Thread.Sleep(800);
                 //semaforo.Abort();
                 //}
                 //for (int i = 0; i < 15;i++)
                 //{
-                this.pictureBox2.Image = global::Semaforo.Properties.Resources.sem_rojo;
-                this.pictureBox3.Image = global::Semaforo.Properties.Resources.sem_rojo;
                 this.pictureBox18.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                this.pictureBox2.Image = global::Semaforo.Properties.Resources.sem_verde;
+                this.pictureBox3.Image = global::Semaforo.Properties.Resources.sem_rojo;
                 //}
+                Thread.Sleep(800);
+
+                this.pictureBox18.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                this.pictureBox2.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                this.pictureBox3.Image = global::Semaforo.Properties.Resources.sem_verde;
+                Thread.Sleep(800);
             }           
         }
         public void Hilo2()
         {
             while (true)
             {
-                for (int i = 0; i < 15;i++)
-                {
+              
                     this.pictureBox21.Image = global::Semaforo.Properties.Resources.sem_verde;
+                    this.pictureBox5.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    this.pictureBox4.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    Thread.Sleep(800);
+
+                    this.pictureBox21.Image = global::Semaforo.Properties.Resources.sem_rojo;
                     this.pictureBox5.Image = global::Semaforo.Properties.Resources.sem_verde;
+                    this.pictureBox4.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    Thread.Sleep(800);
+
+                    this.pictureBox21.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    this.pictureBox5.Image = global::Semaforo.Properties.Resources.sem_rojo;
                     this.pictureBox4.Image = global::Semaforo.Properties.Resources.sem_verde;
-                    Thread.Sleep(400);
-                }
+                    Thread.Sleep(800);
             }
         }
         public void Hilo3()
         {
             while (true)
             {
-                for (int i = 0; i < 15; i++)
-                {
+                
                     this.pictureBox22.Image = global::Semaforo.Properties.Resources.sem_verde;
+                    this.pictureBox7.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    this.pictureBox6.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    Thread.Sleep(800);
+
+                    this.pictureBox22.Image = global::Semaforo.Properties.Resources.sem_rojo;
                     this.pictureBox7.Image = global::Semaforo.Properties.Resources.sem_verde;
+                    this.pictureBox6.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    Thread.Sleep(800);
+
+                    this.pictureBox22.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    this.pictureBox7.Image = global::Semaforo.Properties.Resources.sem_rojo;
                     this.pictureBox6.Image = global::Semaforo.Properties.Resources.sem_verde;
-                    Thread.Sleep(400);
-                }
+                    Thread.Sleep(800);
             }
         }
         public void Hilo4()
         {
             while (true)
             {
-                for (int i = 0; i < 15; i++)
-                {
+                
                     this.pictureBox24.Image = global::Semaforo.Properties.Resources.sem_verde;
+                    this.pictureBox9.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    this.pictureBox8.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    Thread.Sleep(800);
+
+                    this.pictureBox24.Image = global::Semaforo.Properties.Resources.sem_rojo;
                     this.pictureBox9.Image = global::Semaforo.Properties.Resources.sem_verde;
+                    this.pictureBox8.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    Thread.Sleep(800);
+
+                    this.pictureBox24.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    this.pictureBox9.Image = global::Semaforo.Properties.Resources.sem_rojo;
                     this.pictureBox8.Image = global::Semaforo.Properties.Resources.sem_verde;
-                    Thread.Sleep(400);
-                }
+                    Thread.Sleep(800);
             }
         }
         public void Hilo5()
         {
             while (true)
             {
-                for (int i = 0; i < 15; i++)
-                {
-                    this.pictureBox11.Image = global::Semaforo.Properties.Resources.sem_verde;
-                    this.pictureBox10.Image = global::Semaforo.Properties.Resources.sem_verde;
+                
                     this.pictureBox19.Image = global::Semaforo.Properties.Resources.sem_verde;
-                    Thread.Sleep(400);
-                }
+                    this.pictureBox11.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    this.pictureBox10.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    Thread.Sleep(800);
+
+                    this.pictureBox19.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    this.pictureBox11.Image = global::Semaforo.Properties.Resources.sem_verde;
+                    this.pictureBox10.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    Thread.Sleep(800);
+
+                    this.pictureBox11.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    this.pictureBox19.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    this.pictureBox10.Image = global::Semaforo.Properties.Resources.sem_verde;
+                    Thread.Sleep(800);
             }
         }
         public void Hilo6()
         {
             while (true)
             {
-                for (int i = 0; i < 15; i++)
-                {
+                
                     this.pictureBox20.Image = global::Semaforo.Properties.Resources.sem_verde;
+                    this.pictureBox13.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    this.pictureBox12.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    Thread.Sleep(800);
+
+                    this.pictureBox20.Image = global::Semaforo.Properties.Resources.sem_rojo;
                     this.pictureBox13.Image = global::Semaforo.Properties.Resources.sem_verde;
+                    this.pictureBox12.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    Thread.Sleep(800);
+
+                    this.pictureBox20.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    this.pictureBox13.Image = global::Semaforo.Properties.Resources.sem_rojo;
                     this.pictureBox12.Image = global::Semaforo.Properties.Resources.sem_verde;
-                    Thread.Sleep(400);
-                }
+                    Thread.Sleep(800);
             }
         }
         public void Hilo7()
         {
             while (true)
             {
-                for (int i = 0; i < 15; i++)
-                {
+                
                     this.pictureBox23.Image = global::Semaforo.Properties.Resources.sem_verde;
+                    this.pictureBox15.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    this.pictureBox14.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    Thread.Sleep(800);
+
+                    this.pictureBox23.Image = global::Semaforo.Properties.Resources.sem_rojo;
                     this.pictureBox15.Image = global::Semaforo.Properties.Resources.sem_verde;
+                    this.pictureBox14.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    Thread.Sleep(800);
+
+                    this.pictureBox23.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    this.pictureBox15.Image = global::Semaforo.Properties.Resources.sem_rojo;
                     this.pictureBox14.Image = global::Semaforo.Properties.Resources.sem_verde;
-                    Thread.Sleep(400);
-                }
+                    Thread.Sleep(800);
             }
         }
         public void Hilo8()
         {
             while (true)
             {
-                for (int i = 0; i < 15; i++)
-                {
+                
                     this.pictureBox25.Image = global::Semaforo.Properties.Resources.sem_verde;
+                    this.pictureBox17.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    this.pictureBox16.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    Thread.Sleep(800);
+
+                    this.pictureBox25.Image = global::Semaforo.Properties.Resources.sem_rojo;
                     this.pictureBox17.Image = global::Semaforo.Properties.Resources.sem_verde;
+                    this.pictureBox16.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    Thread.Sleep(800);
+
+                    this.pictureBox25.Image = global::Semaforo.Properties.Resources.sem_rojo;
+                    this.pictureBox17.Image = global::Semaforo.Properties.Resources.sem_rojo;
                     this.pictureBox16.Image = global::Semaforo.Properties.Resources.sem_verde;
-                    Thread.Sleep(400);
-                }
+                    Thread.Sleep(800);
             }
         }
  
